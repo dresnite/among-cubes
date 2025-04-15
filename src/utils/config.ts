5f9cb4b9a81cc1bbcf6b05d159cbbb69c87f7e70
Andres Arias <giantquartz@gmail.com>
2025-04-15 14:04:29 +0200

    feat: update config to prepare for launch

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,11 +1,11 @@
-export const MINIMUM_PLAYERS_TO_START_GAME = 3;
-export const COUNTDOWN_TO_START_WITH_MINIMUM_PLAYERS = 5; // seconds
-export const COUNTDOWN_START_GAME = 10; // seconds
+export const MINIMUM_PLAYERS_TO_START_GAME = 4;
+export const COUNTDOWN_TO_START_WITH_MINIMUM_PLAYERS = 45; // seconds
+export const COUNTDOWN_START_GAME = 5; // seconds
 export const BROADCASTER_FREQUENCY = 60; // seconds
-export const ENDING_GAME_DURATION = 10; // seconds
+export const ENDING_GAME_DURATION = 20; // seconds
 export const KNIFE_USE_COOLDOWN = 30; // seconds
-export const TIME_TO_HIDE_EMERGENCY_MEETING_MESSAGE = 5; // seconds
-export const TIME_TO_END_EMERGENCY_MEETING = 10; // seconds
+export const TIME_TO_HIDE_EMERGENCY_MEETING_MESSAGE = 8; // seconds
+export const TIME_TO_END_EMERGENCY_MEETING = 60; // seconds
 
 export const EMERGENCY_BUTTON_ENTITY_NAME = 'emergency-button-entity';
 export const SKIP_VOTE_ENTITY_NAME = 'skip-entity';
